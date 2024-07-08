fa0dbf4de3d13b64a9685bbfe9239e41f57c3339
Harito97 <harito9703@gmail.com>
2024-07-08 22:11:00 +0700

    Refactor ThyroidCancerDataLoader to include classes parameter in get_data_loader method

--- src/data_preparation/ThyroidCancerDataLoader.py
+++ src/data_preparation/ThyroidCancerDataLoader.py
@@ -7,8 +7,8 @@ class ThyroidCancerDataLoader():
         self.transform = transform
         self.balance = balance
 
-    def get_data_loader(self, img_dir, mode, batch_size=32, num_workers=4):
+    def get_data_loader(self, img_dir, classes={0: ["B2"], 1: ["B5", "B6"]}, mode='train', batch_size=32, num_workers=4):
         self.img_dir = img_dir
-        dataset = ThyroidCancerDataset(img_dir=self.img_dir, transform=self.transform, balance=self.balance, mode=mode)
+        dataset = ThyroidCancerDataset(img_dir=self.img_dir, classes={0: ["B2"], 1: ["B5", "B6"]}, transform=self.transform, balance=self.balance, mode=mode)
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=(mode=='train'), num_workers=num_workers)
         return data_loader
